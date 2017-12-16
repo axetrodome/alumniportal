35728f37382c80d990420517770efc9aac56100b
Axel Mhar <axetrodome@yahoo.com>
2017-12-16 22:32:24 +0800

    minor changes

--- users/login.php
+++ users/login.php
@@ -2,8 +2,34 @@
 require_once '../core/init.php';
 $user = new User();
 $login = true;
+$errors = array();
+$loginErrs = [];
 if($user->isLoggedin()){
 	Redirect::to('../views/index.php');
+}
+if(input::exists()){
+	if(Token::check(Input::get('token'))){
+
+		$validate = new Validation();
+		$validation = $validate->check($_POST,array(
+			'username' => array('required' => true),
+			'password' => array('required' => true)
+			));
+		if($validation->passed()){
+			$remember = (Input::get('remember') === 'on') ? true : false;
+			$login = $user->login(Input::get('username'),Input::get('password'), $remember);
+
+			if($login){
+				Session::flash('logged','You are logged in!');
+				Redirect::to('../views/index.php');
+			}else{
+				$loginErrs[] = 'Logging in failed.';
+				$loginErrs[] = 'Wrong Username/Password or Your Account needs approval of the admin.';
+			}
+		}else{
+				$errors = $validation->errors();
+		}
+	}
 }
 ?>
 <!DOCTYPE html>
@@ -15,47 +41,31 @@ if($user->isLoggedin()){
 	<?php require_once '../functions/navigationbar.php' ?>
 	<div style="min-height:500px;" >
 		<form action="" method="post" class="login-form" style="margin-top:150px">
-				<?php
-				if(input::exists()){
-					if(Token::check(Input::get('token'))){
-
-						$validate = new Validation();
-						$validation = $validate->check($_POST,array(
-							'username' => array('required' => true),
-							'password' => array('required' => true)
-							));
-						if($validation->passed()){
-							$remember = (Input::get('remember') === 'on') ? true : false;
-							$login = $user->login(Input::get('username'),Input::get('password'), $remember);
-
-							if($login){
-								Session::flash('logged','You are logged in!');
-								Redirect::to('../views/index.php');
-							}else{
-								echo '<p class="error">Logging in failed.</p>';
-								echo '<small class="error">Wrong Username/Password or Your Account needs approval of the admin.</small>';
-							}
-						}else{
-							foreach ($validation->errors() as $error) {
-								echo $error,'<br>';
-							}
-						}
-					}
-				}
-				?>
-			<div class="input-effect col-3">
-				<input type="text" name="username" id="username" required autocomplete="off" class="input effect-17" value="<?php echo escape(Input::get('username'))?>">
-				<label for="username" class="label">Username</label>
+			<?php foreach($errors as $error): ?>
+			<div class="error">
+				<?php echo $error ?>
+			</div>
+			<?php endforeach; ?>
+			<?php foreach($loginErrs as $loginErr): ?>
+			<div class="error">
+				<?php echo $loginErr ?>
+			</div>
+			<?php endforeach; ?>
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input type="text" name="username" id="username" placeholder="Username" autocomplete="off" class="form-control" value="<?php echo escape(Input::get('username'))?>">
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" name="password" id="password" placeholder="Password"  autocomplete="off" class="form-control" >
 				<span class="focus"></span>
 			</div>
-			<div class="input-effect col-3">
-				<input type="password" name="password" id="password" required autocomplete="off" class="input effect-17">
-				<label for="password" class="label">Password</label>
-				<span class="focus"></span>
+			<div class="form-check">
+			  <label class="form-check-label">
+			    <input type="checkbox" class="form-check-input" name="remember" id="remember">
+			    Remember me
+			  </label>
 			</div>
-			<label for="remember">
-				<input type="checkbox" name="remember" id="remember"> Remember me
-			</label>
 			<div class="divider"></div>
 			<input type="hidden" name="token" value="<?php echo Token::generate() ?>">
 			<input type="submit" value="Login" class="btn-submit">
